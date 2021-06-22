29bd0c527dd38996b5e43e9e8387a1d8af33d016
Ruchika Gosain <58868182+ruchi222@users.noreply.github.com>
2021-06-22 17:32:55 -0400

    Create assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,8 @@
+  const assertArraysEqual = function(arr1,arr2){
+    const result = eqArrays(arr1,arr2)
+    if (eqArrays(array1, array2)) {
+        console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+      } else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+      } 
+    };
