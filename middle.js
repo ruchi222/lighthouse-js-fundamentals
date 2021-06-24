48ddd1e2b2a4ca9a959d9b63c1d815f643776802
Ruchika Gosain <58868182+ruchi222@users.noreply.github.com>
2021-06-24 09:09:51 -0400

    Update middle.js

--- middle.js
+++ middle.js
@@ -37,3 +37,15 @@ const eqArrays = function(array1,array2) {
       return evenMiddle;
     }
   }
+//middle([1]) // => []
+//middle([1, 2]) // => []
+//assertArraysEqual(middle([1]), []);
+//assertArraysEqual(middle([1, 2]), []);
+// middle([1, 2, 3]) // => [2]
+// middle([1, 2, 3, 4, 5]) // => [3]
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+//middle([1, 2, 3, 4]) // => [2, 3]
+//middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+//assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+//assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
