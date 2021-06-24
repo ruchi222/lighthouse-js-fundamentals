15f8048ce2058a79dff47e081c202ccb1019602c
Ruchika Gosain <58868182+ruchi222@users.noreply.github.com>
2021-06-23 22:23:56 -0400

    Create middle.js

---
+++ middle.js
@@ -0,0 +1,14 @@
+const middle = function(array) {
+  if (array.length <= 2) {
+    return [];
+  }
+  if (array.length % 2 !== 0) {
+    let oddMiddle = [];
+    oddMiddle.push(array[Math.floor(array.length / 2)]);
+    return oddMiddle;
+  } else {
+    let evenMiddle = [];
+    evenMiddle.push(array[(array.length / 2) - 1], array[(array.length / 2)]);
+    return evenMiddle;
+  }
+}
