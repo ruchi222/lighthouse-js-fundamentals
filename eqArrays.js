a19347f01da5e18094b657ee7acb54bb9698fb00
Ruchika Gosain <58868182+ruchi222@users.noreply.github.com>
2021-06-28 17:34:31 -0400

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -4,9 +4,14 @@ const eqArrays = function(array1, array2) {
     } else {
       for (let i = 0; i < array1.length; i++) {
         if (array1[i] !== array2[i]) {
-          return false;
+          return console.log(false);
         }
       }
     }
-    return true;
+    return console.log(true);
   };
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
